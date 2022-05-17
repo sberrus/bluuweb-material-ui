de7f263f04a0b1ac1a4de80cbbc239bfadff8ff4
samdev <samuelberrus@gmail.com>
2022-05-17 13:14:36 +0200

    Culminado MaterialUI

--- src/components/MuiGrid/MuiGrid.tsx
+++ src/components/MuiGrid/MuiGrid.tsx
@@ -1,29 +1,49 @@
-import { Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 
 const MuiGrid = () => {
-	const Item = ({ children }) => <div>{children}</div>;
+	const containerStyle = {
+		border: "5px solid #000",
+		textAlign: "center",
+	};
+	const itemStyle = {
+		height: "5em",
+		border: "1px solid red",
+	};
+
 	return (
 		<>
 			<Typography variant="h2">MuiGrid</Typography>
 			<p>Aprendiendo a usar grid en MUI</p>
 			<p>
 				Similar a como se trabaja con los grid de Bootstrap. Es básicamente un componente para declarar un
-				componente padre como flex container.
+				componente padre como flex container que se comporte como un grid de 12 columnas.
 			</p>
-			<Grid container columnSpacing={3} rowSpacing={3}>
-				<Grid item xs={6}>
-					Elemento #1
+			<p>
+				El componente <i>Grid</i> tiene que especificar mediante props si es un <i>item</i> o si es un{" "}
+				<i>container</i>. siendo esos los respectivos props <i>item, container</i>
+			</p>
+			<Container style={{ marginTop: "2em" }}>
+				<Grid container columnSpacing={3} rowSpacing={3} style={containerStyle}>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 12
+					</Grid>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 6
+					</Grid>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 6
+					</Grid>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 4
+					</Grid>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 4
+					</Grid>
+					<Grid item xs={12} md={6} lg={4} style={itemStyle}>
+						xs 4
+					</Grid>
 				</Grid>
-				<Grid item xs={6}>
-					Elemento #2
-				</Grid>
-				<Grid item xs={6}>
-					Elemento #3
-				</Grid>
-				<Grid item xs={6}>
-					Elemento #4
-				</Grid>
-			</Grid>
+			</Container>
 		</>
 	);
 };
